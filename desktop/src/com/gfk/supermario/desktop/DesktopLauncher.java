b22cf083509db6732c50f0eaa6a53a395b85d0c2
Olav Markus Sjursø <olavms16@student.uia.no>
2017-04-18 13:53:00 +0200

    Set up LibGDX library and modified gitignore

---
+++ desktop/src/com/gfk/supermario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gfk.supermario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gfk.supermario.supermario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new supermario(), config);
+	}
+}
