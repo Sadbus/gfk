3a9090d89993c1270dbd5e518fba41b68f0c447a
Morten Mikalsen <mortem16@uia.no>
2017-04-20 11:07:26 +0200

    game state

--- core/src/com/gfk/supermario/SuperMario.java
+++ core/src/com/gfk/supermario/SuperMario.java
@@ -1,21 +1,36 @@
 package com.gfk.supermario;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.gfk.supermario.Screens.PlayScreen;
+import com.gfk.supermario.States.GameStateManager;
+import com.gfk.supermario.States.MenuState;
 
-public class SuperMario extends Game {
-	public SpriteBatch batch;
-	
+public class SuperMario extends Game
+{
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 800;
+    public static final String TITLE = "Games For Kekistan";
+
+    private GameStateManager gsm;
+    private SpriteBatch batch;
+
+
 	@Override
-	public void create () {
+	public void create ()
+	{
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
+		gsm = new GameStateManager();
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        gsm.push(new MenuState(gsm));
 	}
 
 	@Override
-	public void render () {
-		super.render();
-	}
-
+    public void render()
+    {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gsm.update(Gdx.graphics.getDeltaTime());
+        gsm.render(batch);
+    }
 }
--- desktop/src/com/gfk/supermario/desktop/DesktopLauncher.java
+++ desktop/src/com/gfk/supermario/desktop/DesktopLauncher.java
@@ -4,9 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gfk.supermario.SuperMario;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = SuperMario.WIDTH;
+		config.height = SuperMario.HEIGHT;
+		config.title = SuperMario.TITLE;
 		new LwjglApplication(new SuperMario(), config);
 	}
 }
