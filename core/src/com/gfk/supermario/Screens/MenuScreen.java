26c68aa15af1dd3799a0e997df339483fb2b02dc
Morten Mikalsen <mortem16@uia.no>
2017-04-20 13:32:49 +0200

    Changed code structure, switching between MenuScreen -> GameScreen works

---
+++ core/src/com/gfk/supermario/Screens/MenuScreen.java
@@ -0,0 +1,76 @@
+package com.gfk.supermario.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.gfk.supermario.GameRenderer;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class MenuScreen implements Screen
+{
+
+    final GameRenderer game;
+
+    OrthographicCamera camera;
+
+    public MenuScreen(final GameRenderer game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta)
+    {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to ", 400, 280);
+        game.font.draw(game.batch, "the proud nation of Kekistan", 400, 200);
+        game.batch.end();
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
+        {
+            game.setScreen(new GameScreen(game));
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
