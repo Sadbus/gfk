686b077ad224560073ea750960a8c5fa31a5115a
Olav Markus Sjursø <olavms16@student.uia.no>
2017-04-21 16:59:53 +0200

    Started working on Box2D, applied gravity to world and mass to a sprite

--- core/src/com/gfk/supermario/Screens/GameScreen.java
+++ core/src/com/gfk/supermario/Screens/GameScreen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.gfk.supermario.GameRenderer;
 
 /**
@@ -16,13 +20,49 @@ public class GameScreen implements Screen{
 
     Music shootingStars;
 
+    World world;
+    Body body;
+    Texture img;
+    Sprite sprite;
+
+
     public GameScreen(GameRenderer game)
     {
         this.game = game;
         texture = new Texture("badlogic.jpg");
         shootingStars = Gdx.audio.newMusic(Gdx.files.internal("mario.mp3"));
         shootingStars.setLooping(true);
+
+        img = new Texture("mario.png");
+        sprite = new Sprite(img);
+        sprite.setScale(5);
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+
+
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+
+        BodyDef bodydef = new BodyDef();
+        bodydef.type = BodyDef.BodyType.DynamicBody;
+
+        body = world.createBody(bodydef);
+
+        PolygonShape shape = new PolygonShape();
+
+        shape.setAsBox(sprite.getWidth()/2, sprite.getHeight()/2);
+
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        shape.dispose();
+
     }
+
+
 
     @Override
     public void show() {
@@ -38,6 +78,17 @@ public class GameScreen implements Screen{
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
+
+        world.step(1/60f, 6, 2);
+
+        sprite.setPosition(body.getPosition().x, body.getPosition().y);
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(sprite, sprite.getX(), sprite.getY());
+        game.batch.end();
+
     }
 
     @Override
@@ -63,6 +114,9 @@ public class GameScreen implements Screen{
     @Override
     public void dispose() {
         shootingStars.dispose();
+
+        img.dispose();
+        world.dispose();
 
     }
 }
