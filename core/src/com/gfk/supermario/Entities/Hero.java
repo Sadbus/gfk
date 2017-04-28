f63948d418d3bcefb858d0c78bf760dc22995404
Morten Mikalsen <mortem16@uia.no>
2017-04-28 11:34:42 +0200

    Added hero animation

--- core/src/com/gfk/supermario/Entities/Hero.java
+++ core/src/com/gfk/supermario/Entities/Hero.java
@@ -1,12 +1,16 @@
 package com.gfk.supermario.Entities;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import com.gfk.supermario.GameRenderer;
 import com.gfk.supermario.Screens.GameScreen;
+
+import javax.swing.plaf.nimbus.State;
 
 /**
  * Created by olav on 20.04.17.
@@ -15,17 +19,49 @@ public class Hero extends Sprite
 {
     public World world;
     public Body b2body;
+    public enum State
+    {
+        FALLING, JUMPING, STANDING, RUNNING
+    };
+    public State currentState;
+    public State previousState;
+    private Animation heroRun;
+    private Animation heroJump;
+
+    private float stateTimer;
+    private boolean runningRight;
 
     public static Boolean hasKey;
 
     private TextureRegion heroStand;
+
     public Hero(World world, GameScreen screen)
     {
         super(screen.getAtlas().findRegion("Still"));
         this.world = world;
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for (int i = 0; i < 1; i++)
+        {
+            frames.add(new TextureRegion(getTexture(), i * 24, 0, 20, 24 ));
+        }
+        heroRun = new Animation(0.1f, frames);
+        frames.clear();
+
+        for (int i = 2; i < 3; i++)
+        {
+            frames.add(new TextureRegion(getTexture(), i*24, 0, 20, 24));
+        }
+        heroJump = new Animation(0.1f, frames);
+
+
+        heroStand = new TextureRegion(getTexture(), 133,1,20,24);
         defineHero();
-        heroStand = new TextureRegion(getTexture(), 0,0,24,24);
-        setBounds(0,0, 24/ GameRenderer.PPM, 24/GameRenderer.PPM);
+        setBounds(5,0, 20/ GameRenderer.PPM, 24/GameRenderer.PPM);
         setRegion(heroStand);
 
         hasKey = false;
@@ -35,6 +71,69 @@ public class Hero extends Sprite
     public void update(float dt)
     {
         setPosition(b2body.getPosition().x - getWidth()/2, b2body.getPosition().y - getHeight()/2);
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt)
+    {
+        currentState = getState();
+        TextureRegion region;
+        switch (currentState)
+        {
+            case JUMPING:
+            {
+                region = (TextureRegion) heroJump.getKeyFrame(stateTimer);
+                break;
+            }
+            case RUNNING:
+            {
+                region = (TextureRegion) heroRun.getKeyFrame(stateTimer, true);
+                break;
+            }
+            case FALLING:
+            case STANDING:
+            default:
+            {
+                region = heroStand;
+                break;
+            }
+        }
+
+        // Going left or right
+
+        if((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX())
+        {
+            region.flip(true, false);
+            runningRight = false;
+        }
+        else if ((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX())
+        {
+            region.flip(true, false);
+            runningRight = true;
+        }
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        return region;
+    }
+
+    public State getState()
+    {
+        if (b2body.getLinearVelocity().y > 0) //|| (b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING))
+        {
+            return State.JUMPING;
+        }
+        else if (b2body.getLinearVelocity().y < 0)
+        {
+            return State.FALLING;
+        }
+        else if (b2body.getLinearVelocity().x != 0)
+        {
+            return State.RUNNING;
+        }
+        else
+        {
+            return State.STANDING;
+        }
     }
 
     public void defineHero()
@@ -46,7 +145,7 @@ public class Hero extends Sprite
 
         FixtureDef fixtureDef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5 / GameRenderer.PPM);
+        shape.setRadius(10 / GameRenderer.PPM);
         fixtureDef.filter.categoryBits = GameRenderer.HERO_BIT;
         fixtureDef.filter.maskBits = GameRenderer.DEFAULT_BIT |
                 GameRenderer.COIN_BIT |
@@ -60,7 +159,7 @@ public class Hero extends Sprite
 
         // Definerer sensor for å sjekke kollisjon med hodet
         EdgeShape head = new EdgeShape();
-        head.set(new Vector2(-2 / GameRenderer.PPM, 6 / GameRenderer.PPM), new Vector2(2 / GameRenderer.PPM, 6 / GameRenderer.PPM));
+        head.set(new Vector2(-2 / GameRenderer.PPM, 10 / GameRenderer.PPM), new Vector2(2 / GameRenderer.PPM, 10 / GameRenderer.PPM));
         fixtureDef.filter.categoryBits = GameRenderer.HERO_HEAD_BIT;
         fixtureDef.shape = head;
         fixtureDef.isSensor = true;
@@ -90,6 +189,6 @@ public class Hero extends Sprite
 
     public void draw(Batch batch)
     {
-
+        super.draw(batch);
     }
 }
